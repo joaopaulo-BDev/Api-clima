ebcee04668a56e82e74f490e42468afb2bb3307e
jhowsooz <paulomattos.jm@icloud.com>
2026-02-18 12:02:13 -0300

    commit star npm

--- index.js
+++ index.js
@@ -13,25 +13,30 @@ app.get('/clima', async (req, res) => {
     return res.status(400).json({ mensagem: 'Faltando OPENWEATHER_API_KEY. Verifique seu .env' });
   }
 
-  try {
-    const resposta = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cidade)}&appid=${apiKey}&units=metric&lang=pt_br`
-    );
-    res.json(resposta.data);
+ try {
+   const resposta = await axios.get(
+     `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cidade)}&appid=${apiKey}&units=metric&lang=pt_br`
+   );
 
-     res.json({
-        cidade: dados.name,
-        pais: dados.sys.country,
-        temperatura: dados.main.temp,
-        sensacao_termica: dados.main.feels_like,
-        umidade: dados.main.humidity,
-        descricao: dados.weather[0].description,
-        vento: dados.wind.speed });
+   const dados = resposta.data;
 
-  } catch (erro) {
-    console.error(erro.message);
-    res.status(500).json({ mensagem: 'Erro ao consultar clima' });
-  }
+   return res.json({
+     cidade: dados.name,
+     pais: dados.sys.country,
+     temperatura: dados.main.temp,
+     sensacao_termica: dados.main.feels_like,
+     umidade: dados.main.humidity,
+     descricao: dados.weather[0].description,
+     vento: dados.wind.speed
+   });
+
+ } catch (erro) {
+   console.error(erro.message);
+
+   return res.status(500).json({
+     mensagem: 'Erro ao consultar clima'
+   });
+ }
 });
 
 app.listen(PORT, () => {
